5b966c03f2d88ea0a3d611e0983d18bd97dd9d4b
Patrīcija Lasmane <124577162+patricijalas@users.noreply.github.com>
2023-02-19 12:24:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,29 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
+   pos=0
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i))
+            pos=pos+1
             # Process opening bracket, write your code here
-            pass
-
+            
         if next in ")]}":
+            if pos==0 and i==0:
+                return 1
+            if pos>0 and are_matching(opening_brackets_stack [pos-1].char,next):
+                del opening_brackets_stack [-1]
+                pos=pos-1
+            else:
+                return i+1
             # Process closing bracket, write your code here
-            pass
+            
+    if pos==0:
+        return 'Success'
+    else:
+        return opening_brackets_stack [-1].position+1
+
 
 
 def main():
